2d8c196442ed94675148ecfa19b21a815e6ca7c9
wootaepark <pakdal99@gmail.com>
2024-11-04 23:13:57 +0900

    feat : Order 주문 상태 변경, 취소 구현

--- src/main/java/com/sparta/deliverybackend/domain/order/controller/OrderManagerController.java
+++ src/main/java/com/sparta/deliverybackend/domain/order/controller/OrderManagerController.java
@@ -1,47 +1,39 @@
 package com.sparta.deliverybackend.domain.order.controller;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sparta.deliverybackend.api.controller.dto.VerifiedMember;
 import com.sparta.deliverybackend.domain.order.controller.dto.OrderCancelResponseDto;
-import com.sparta.deliverybackend.domain.order.controller.dto.OrderMenuDto;
-import com.sparta.deliverybackend.domain.order.controller.dto.OrderRequestDto;
-import com.sparta.deliverybackend.domain.order.controller.dto.OrderResponseDto;
+import com.sparta.deliverybackend.domain.order.controller.dto.OrderUpdateResponseDto;
 import com.sparta.deliverybackend.domain.order.service.OrderService;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
-public class OrderController {
+@RequestMapping("/api/manager")
+public class OrderManagerController {
 
 	private final OrderService orderService;
 
-	@PostMapping("/order")
-	public ResponseEntity<OrderResponseDto> createOrder(
-		// 주문 요청 (손님 전용)
+	// 주문 상황 업데이트 (가게 사장님 전용), 이 요청 자체를 수락으로 봄
+	// AOP 에 의해 로그 남길 예정 (요청 시각, 가계 id 주문 id)
+	// 주문 완료 시 orders_menu 테이블에서 삭제 예정
+	@PatchMapping("/order/{orderId}")
+	public ResponseEntity<OrderUpdateResponseDto> updateOrderStatus(
 		VerifiedMember verifiedMember,
-		@Valid @RequestBody OrderRequestDto requestDto
+		@PathVariable Long orderId
 	) {
-
-		List<OrderMenuDto> orderMenu = requestDto.getMenus();
-		return ResponseEntity
-			.status(HttpStatus.CREATED)
-			.body(orderService.createOrder(verifiedMember, orderMenu));
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(orderService.updateOrderStatus(verifiedMember, orderId));
 	}
 
-	// 주문 취소는 상태가 WAIT 상태일 때만 취소 가능 (손님, 사장 모두)
 	@DeleteMapping("/order/{orderId}")
 	public ResponseEntity<OrderCancelResponseDto> deleteOrder(
 		VerifiedMember verifiedMember,
@@ -50,5 +42,4 @@ public class OrderController {
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(orderService.cancelOrder(verifiedMember, orderId));
 	}
-
 }
--- src/main/java/com/sparta/deliverybackend/domain/order/repository/OrderMenuRepository.java
+++ src/main/java/com/sparta/deliverybackend/domain/order/repository/OrderMenuRepository.java
@@ -1,8 +1,12 @@
 package com.sparta.deliverybackend.domain.order.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sparta.deliverybackend.domain.order.entity.OrderMenu;
 
 public interface OrderMenuRepository extends JpaRepository<OrderMenu, Long> {
+	Optional<OrderMenu> findFirstByOrderId(Long orderId);
+
 }
