9e2d92c03b6f28091e74f1c00a4cf3b1fde15d98
Dohyeon_Parrk <parkyj3213@naver.com>
2024-11-04 15:14:50 +0900

    fix : 메뉴 조회 로직

--- src/main/java/com/sparta/deliverybackend/domain/restaurant/controller/RestaurantController.java
+++ src/main/java/com/sparta/deliverybackend/domain/restaurant/controller/RestaurantController.java
@@ -1,17 +1,18 @@
 package com.sparta.deliverybackend.domain.restaurant.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.sparta.deliverybackend.api.controller.dto.VerifiedMember;
-import com.sparta.deliverybackend.domain.member.entity.Manager;
 import com.sparta.deliverybackend.domain.restaurant.controller.dto.RestaurantCreateRepDto;
 import com.sparta.deliverybackend.domain.restaurant.controller.dto.RestaurantCreateReqDto;
 import com.sparta.deliverybackend.domain.restaurant.service.RestaurantService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,5 +36,4 @@ public class RestaurantController {
 //                .status(HttpStatus.OK)
 //                .body(response);
 //    }
-
 }
