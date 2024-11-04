e49939db4eefbf7c5d9cef5f717970ffaa8575e3
siyeon02 <chrysapark@cau.ac.kr>
2024-11-04 16:40:07 +0900

    test(restaurant): RestaurantService 테스트 코드 작성 #12

--- src/main/java/com/sparta/deliverybackend/domain/restaurant/service/RestaurantService.java
+++ src/main/java/com/sparta/deliverybackend/domain/restaurant/service/RestaurantService.java
@@ -9,20 +9,15 @@ import com.sparta.deliverybackend.domain.restaurant.repository.RestaurantReposit
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.filter.OrderedFormContentFilter;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class RestaurantService {
     private final RestaurantRepository restaurantRepository;
     private final ManagerRepository managerRepository;
-    private final OrderedFormContentFilter formContentFilter;
 
     public RestaurantCreateRepDto createRestaurant(RestaurantCreateReqDto reqDto, VerifiedMember verifiedMember) {//로그인 유저로 바꿀 예정
         Manager manager = managerRepository.findById(verifiedMember.id())
