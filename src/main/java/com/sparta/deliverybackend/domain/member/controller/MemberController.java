e6e97720885d18a4bdc90777ab061dec99bc9a13
kimwanyoung <dhks2869@gmail.com>
2024-11-04 12:32:41 +0900

    feat : MemberController update, delete 메서드 추가

---
+++ src/main/java/com/sparta/deliverybackend/domain/member/controller/MemberController.java
@@ -0,0 +1,38 @@
+package com.sparta.deliverybackend.domain.member.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sparta.deliverybackend.api.controller.dto.VerifiedMember;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+	private final MemberService memberService;
+
+	@PatchMapping("/{id}")
+	public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody MemberUpdateReqDto req) {
+		memberService.update(req);
+		return ResponseEntity
+			.status(HttpStatus.NO_CONTENT)
+			.build();
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id, VerifiedMember verifiedMember) {
+		memberService.delete(id, verifiedMember);
+		return ResponseEntity
+			.status(HttpStatus.NO_CONTENT)
+			.build();
+	}
+}
