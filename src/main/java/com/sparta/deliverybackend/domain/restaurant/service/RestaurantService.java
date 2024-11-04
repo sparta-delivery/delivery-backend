902c809189ae9ede2bcfedc1e0eb6f1ece35bd39
siyeon02 <chrysapark@cau.ac.kr>
2024-11-04 11:28:32 +0900

    feat(restaurant): 가게 생성 기능 완료 #1

--- src/main/java/com/sparta/deliverybackend/domain/restaurant/service/RestaurantService.java
+++ src/main/java/com/sparta/deliverybackend/domain/restaurant/service/RestaurantService.java
@@ -1,5 +1,6 @@
 package com.sparta.deliverybackend.domain.restaurant.service;
 
+import com.sparta.deliverybackend.api.controller.dto.VerifiedMember;
 import com.sparta.deliverybackend.domain.member.entity.Manager;
 import com.sparta.deliverybackend.domain.member.repository.ManagerRepository;
 import com.sparta.deliverybackend.domain.restaurant.controller.dto.RestaurantCreateRepDto;
@@ -8,7 +9,8 @@ import com.sparta.deliverybackend.domain.restaurant.entity.Restaurant;
 import com.sparta.deliverybackend.domain.restaurant.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +18,7 @@ public class RestaurantService {
     private final RestaurantRepository restaurantRepository;
     private final ManagerRepository managerRepository;
 
-    public RestaurantCreateRepDto createRestaurant(RestaurantCreateReqDto reqDto, Manager managerId) {//로그인 유저로 바꿀 예정
+    public RestaurantCreateRepDto createRestaurant(RestaurantCreateReqDto reqDto, VerifiedMember verifiedMember) {//로그인 유저로 바꿀 예정
         Manager manager = managerRepository.findById(reqDto.getManagerId())
                 .orElseThrow(()->new IllegalArgumentException("존재하지 않는 사장님 아이디입니다."));
 
@@ -24,4 +26,9 @@ public class RestaurantService {
         Restaurant savedRestaurant = restaurantRepository.save(restaurant);
         return new RestaurantCreateRepDto(savedRestaurant);
     }
+
+//    public List<RestaurantCreateRepDto> getRestaurants(VerifiedMember verifiedMember) {
+//
+//        return
+//    }
 }
