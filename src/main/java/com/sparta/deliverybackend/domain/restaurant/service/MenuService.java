ea3eaa95211c6dcc4f39f896596bff468cd115d0
Dohyeon_Parrk <parkyj3213@naver.com>
2024-11-05 16:07:52 +0900

    feat : 메뉴 옵션 추가, 수정 메소드 & 조회 기능 추가 (JSON 문자열 형태로 데이터 db에 저장)

--- src/main/java/com/sparta/deliverybackend/domain/restaurant/service/MenuService.java
+++ src/main/java/com/sparta/deliverybackend/domain/restaurant/service/MenuService.java
@@ -1,14 +1,18 @@
 package com.sparta.deliverybackend.domain.restaurant.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.sparta.deliverybackend.api.controller.dto.VerifiedMember;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sparta.deliverybackend.api.auth.controller.dto.VerifiedMember;
 import com.sparta.deliverybackend.domain.member.entity.Manager;
 import com.sparta.deliverybackend.domain.member.repository.ManagerRepository;
 import com.sparta.deliverybackend.domain.restaurant.controller.dto.MenuCreateReqDto;
 import com.sparta.deliverybackend.domain.restaurant.controller.dto.MenuRespDto;
 import com.sparta.deliverybackend.domain.restaurant.controller.dto.MenuUpdateReqDto;
+import com.sparta.deliverybackend.domain.restaurant.controller.dto.OptionReqDto;
 import com.sparta.deliverybackend.domain.restaurant.entity.CuisineType;
 import com.sparta.deliverybackend.domain.restaurant.entity.Menu;
 import com.sparta.deliverybackend.domain.restaurant.entity.Restaurant;
@@ -76,6 +80,38 @@ public class MenuService {
 		}
 
 		menu.isDeleted();
+		menuRepository.save(menu);
+	}
+
+	@Transactional(readOnly = true)
+	public List<OptionReqDto> getMenuOptions(Long menuId){
+		Menu menu = menuRepository.findById(menuId)
+			.orElseThrow(() -> new EntityNotFoundException("메뉴를 찾을 수 없습니다."));
+
+		try{
+			return menu.getOptions();
+		} catch (JsonProcessingException e){
+			throw new RuntimeException("옵션 조회 중 오류가 발생했습니다.", e);
+		}
+	}
+
+	@Transactional
+	public void updateMenuOptions(Long restaurantId, Long menuId, List<OptionReqDto> optionReqDtos, VerifiedMember verifiedMember) {
+		Restaurant restaurant = restaurantRepository.findById(restaurantId)
+			.orElseThrow(() -> new EntityNotFoundException("가게를 찾을 수 없습니다."));
+
+		Manager manager = managerRepository.findById(verifiedMember.id())
+			.orElseThrow(() -> new EntityNotFoundException("메뉴를 생성할 권한이 없습니다."));
+
+		Menu menu = menuRepository.findById(menuId)
+			.orElseThrow(() -> new EntityNotFoundException("메뉴를 찾을 수 없습니다."));
+
+		if(!menu.getRestaurant().getId().equals(restaurant.getId())){
+			throw new IllegalArgumentException("해당 가게의 메뉴가 아닙니다. 다시 확인해주세요");
+		}
+
+		menu.updateOptions(optionReqDtos);
+
 		menuRepository.save(menu);
 	}
 }
