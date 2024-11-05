de925d2efbfdbccd10f24f0de86214bde856cc9d
kimwanyoung <dhks2869@gmail.com>
2024-11-05 20:35:55 +0900

    refactor : 소셜 로그인 리팩토링

--- src/main/java/com/sparta/deliverybackend/api/oauth2/config/InMemoryProviderRepository.java
+++ src/main/java/com/sparta/deliverybackend/api/oauth2/config/InMemoryProviderRepository.java
@@ -3,16 +3,17 @@ package com.sparta.deliverybackend.api.oauth2.config;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.sparta.deliverybackend.api.oauth2.provider.Oauth2Provider;
+import com.sparta.deliverybackend.api.oauth2.provider.Oauth2ProviderProperties;
+import com.sparta.deliverybackend.api.oauth2.service.Oauth2ProviderType;
 
 public class InMemoryProviderRepository {
-	private final Map<String, Oauth2Provider> providers;
+	private final Map<Oauth2ProviderType, Oauth2ProviderProperties> providers;
 
-	public InMemoryProviderRepository(Map<String, Oauth2Provider> providers) {
+	public InMemoryProviderRepository(Map<Oauth2ProviderType, Oauth2ProviderProperties> providers) {
 		this.providers = new HashMap<>(providers);
 	}
 
-	public Oauth2Provider findProvider(String name) {
+	public Oauth2ProviderProperties findProvider(Oauth2ProviderType name) {
 		return providers.get(name);
 	}
 }
