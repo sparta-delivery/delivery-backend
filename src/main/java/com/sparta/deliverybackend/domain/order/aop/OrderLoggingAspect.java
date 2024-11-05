8532deb73a998a735333669e3c73f6ec2ef217f8
wootaepark <pakdal99@gmail.com>
2024-11-05 16:50:23 +0900

    refactor : ã취소AOP 취소 로거 추가

--- src/main/java/com/sparta/deliverybackend/domain/order/aop/OrderLoggingAspect.java
+++ src/main/java/com/sparta/deliverybackend/domain/order/aop/OrderLoggingAspect.java
@@ -7,9 +7,9 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import com.sparta.deliverybackend.domain.order.controller.dto.OrderCancelResponseDto;
 import com.sparta.deliverybackend.domain.order.controller.dto.OrderResponseDto;
 import com.sparta.deliverybackend.domain.order.controller.dto.OrderUpdateResponseDto;
-import com.sparta.deliverybackend.domain.order.entity.OrderStatus;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,23 +18,21 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class OrderLoggingAspect {
 
-	@AfterReturning(value = "execution(* com.sparta.deliverybackend.domain.order.service.OrderService.createOrder(..))", returning = "result")
-	public void logAfterCreateOrder(JoinPoint joinPoint, Object result) {
+	@AfterReturning(value = "execution(* com.sparta.deliverybackend.domain.order.service.OrderService.*Order(..))", returning = "result")
+	public void logAfterOrderMethods(JoinPoint joinPoint, Object result) {
+		LocalDateTime requestTime = LocalDateTime.now();
+
 		if (result instanceof OrderResponseDto orderResponse) {
-			String restaurantName = orderResponse.getRestaurantName();
-			OrderStatus orderStatus = orderResponse.getOrderStatus();
-
-			log.info("주문 생성 - 요청 시각: {}, 가게 이름: {}, 주문 상태: {}", LocalDateTime.now(), restaurantName, orderStatus);
+			log.info("주문 생성 - 요청 시각: {}, 가게 이름: {}, 주문 상태: {}",
+				requestTime, orderResponse.getRestaurantName(), orderResponse.getOrderStatus());
 		}
-	}
-
-	@AfterReturning(value = "execution(* com.sparta.deliverybackend.domain.order.service.OrderService.updateOrderStatus(..))", returning = "result")
-	public void logAfterUpdateOrderStatus(JoinPoint joinPoint, Object result) {
 		if (result instanceof OrderUpdateResponseDto orderUpdateResponse) {
-			Long orderId = orderUpdateResponse.getOrderId();
-			OrderStatus orderStatus = orderUpdateResponse.getStatus();
-
-			log.info("주문 상태 변경 - 요청 시각: {}, 주문 ID: {}, 주문 상태: {}", LocalDateTime.now(), orderId, orderStatus);
+			log.info("주문 상태 변경 - 요청 시각: {}, 주문 ID: {}, 주문 상태: {}",
+				requestTime, orderUpdateResponse.getOrderId(), orderUpdateResponse.getStatus());
+		}
+		if (result instanceof OrderCancelResponseDto orderCancelResponse) {
+			log.info("주문 취소 - 요청 시각: {}, 주문 ID: {}, 주문 상태: {}",
+				requestTime, orderCancelResponse.getOrderId(), orderCancelResponse.getStatus());
 		}
 	}
 }
