b7daadd28a4edbb780afa84aa50b43d5954dea9e
wootaepark <pakdal99@gmail.com>
2024-11-05 16:17:37 +0900

    feat : 주문 요청, 상태 변경 시 로거 구현

---
+++ src/main/java/com/sparta/deliverybackend/domain/order/aop/OrderLoggingAspect.java
@@ -0,0 +1,40 @@
+package com.sparta.deliverybackend.domain.order.aop;
+
+import java.time.LocalDateTime;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import com.sparta.deliverybackend.domain.order.controller.dto.OrderResponseDto;
+import com.sparta.deliverybackend.domain.order.controller.dto.OrderUpdateResponseDto;
+import com.sparta.deliverybackend.domain.order.entity.OrderStatus;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+@Component
+public class OrderLoggingAspect {
+
+	@AfterReturning(value = "execution(* com.sparta.deliverybackend.domain.order.service.OrderService.createOrder(..))", returning = "result")
+	public void logAfterCreateOrder(JoinPoint joinPoint, Object result) {
+		if (result instanceof OrderResponseDto orderResponse) {
+			String restaurantName = orderResponse.getRestaurantName();
+			OrderStatus orderStatus = orderResponse.getOrderStatus();
+
+			log.info("주문 생성 - 요청 시각: {}, 가게 이름: {}, 주문 상태: {}", LocalDateTime.now(), restaurantName, orderStatus);
+		}
+	}
+
+	@AfterReturning(value = "execution(* com.sparta.deliverybackend.domain.order.service.OrderService.updateOrderStatus(..))", returning = "result")
+	public void logAfterUpdateOrderStatus(JoinPoint joinPoint, Object result) {
+		if (result instanceof OrderUpdateResponseDto orderUpdateResponse) {
+			Long orderId = orderUpdateResponse.getOrderId();
+			OrderStatus orderStatus = orderUpdateResponse.getStatus();
+
+			log.info("주문 상태 변경 - 요청 시각: {}, 주문 ID: {}, 주문 상태: {}", LocalDateTime.now(), orderId, orderStatus);
+		}
+	}
+}
