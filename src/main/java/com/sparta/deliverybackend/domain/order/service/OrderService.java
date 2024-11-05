2bc99cf98a77574d7101e2de83c5e8f63c012cfd
Dohyeon_Parrk <parkyj3213@naver.com>
2024-11-05 15:12:43 +0900

    fix : verifiedmember 관련 디렉토리 구조 변경 -> 임포트 수정

--- src/main/java/com/sparta/deliverybackend/domain/order/service/OrderService.java
+++ src/main/java/com/sparta/deliverybackend/domain/order/service/OrderService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.sparta.deliverybackend.api.controller.dto.VerifiedMember;
+import com.sparta.deliverybackend.api.auth.controller.dto.VerifiedMember;
 import com.sparta.deliverybackend.domain.member.entity.Manager;
 import com.sparta.deliverybackend.domain.member.entity.Member;
 import com.sparta.deliverybackend.domain.member.repository.ManagerRepository;
