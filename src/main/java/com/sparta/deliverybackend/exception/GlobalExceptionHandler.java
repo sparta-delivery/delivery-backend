fb9e8015257af0f555b7b18e2a1d8fe627d7cac4
wootaepark <pakdal99@gmail.com>
2024-11-06 15:30:22 +0900

    feat : requestDto validation exception

--- src/main/java/com/sparta/deliverybackend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/deliverybackend/exception/GlobalExceptionHandler.java
@@ -1,6 +1,12 @@
 package com.sparta.deliverybackend.exception;
 
+import static com.sparta.deliverybackend.exception.enums.ExceptionCode.*;
+
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,6 +15,7 @@ import com.sparta.deliverybackend.exception.customException.NotFoundEntityExcept
 import com.sparta.deliverybackend.exception.customException.NotHaveAuthorityException;
 import com.sparta.deliverybackend.exception.customException.OrderPriceMismatchingException;
 import com.sparta.deliverybackend.exception.dto.ExceptionRespDto;
+import com.sparta.deliverybackend.exception.dto.NotValidRequestParameterDto;
 import com.sparta.deliverybackend.exception.enums.ExceptionCode;
 
 import lombok.extern.slf4j.Slf4j;
@@ -49,6 +56,29 @@ public class GlobalExceptionHandler {
 		return ExceptionRespDto.builder()
 			.code(exceptionCode.name())
 			.message(exceptionCode.getMessage())
+			.build();
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		ExceptionCode exceptionCode = INVALID_REQUEST_PARAMETER;
+		log.error("{}: {}", exceptionCode, exceptionCode.getMessage());
+		return ResponseEntity.status(exceptionCode.getHttpStatus())
+			.body(makeNotValidRequestParameter(e, exceptionCode));
+	}
+
+	private NotValidRequestParameterDto makeNotValidRequestParameter(BindException e,
+		ExceptionCode exceptionCode) {
+		List<NotValidRequestParameterDto.NotValidParameter> notValidParameters = e.getBindingResult()
+			.getFieldErrors()
+			.stream()
+			.map(NotValidRequestParameterDto.NotValidParameter::of)
+			.toList();
+
+		return NotValidRequestParameterDto.builder()
+			.code(exceptionCode.name())
+			.message(exceptionCode.getMessage())
+			.notValidParameters(notValidParameters)
 			.build();
 	}
 
