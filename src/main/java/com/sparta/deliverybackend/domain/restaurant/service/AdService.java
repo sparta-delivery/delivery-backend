2aa96f8473584de04b051c1e129fe4a514857087
Dohyeon_Parrk <parkyj3213@naver.com>
2024-11-06 11:55:33 +0900

    fix : verifiedMember 검증 추가 & Ad 검증 공통으로 분리

--- src/main/java/com/sparta/deliverybackend/domain/restaurant/service/AdService.java
+++ src/main/java/com/sparta/deliverybackend/domain/restaurant/service/AdService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sparta.deliverybackend.api.auth.controller.dto.VerifiedMember;
+import com.sparta.deliverybackend.domain.member.entity.Manager;
+import com.sparta.deliverybackend.domain.member.repository.ManagerRepository;
 import com.sparta.deliverybackend.domain.restaurant.controller.dto.AdReqDto;
 import com.sparta.deliverybackend.domain.restaurant.controller.dto.AdRespDto;
 import com.sparta.deliverybackend.domain.restaurant.entity.Ad;
@@ -24,11 +26,25 @@ public class AdService {
 
 	private final AdRepository adRepository;
 	private final RestaurantRepository restaurantRepository;
+	private final ManagerRepository managerRepository;
+
+	// 광고 조회 검증
+	private Ad findAdOrThrow(Long adId){
+		return adRepository.findById(adId)
+			.orElseThrow(() -> new EntityNotFoundException("광고를 찾을 수 없습니다."));
+	}
 
 	@Transactional
 	public AdRespDto createAd(AdReqDto adReqDto, VerifiedMember verifiedMember) {
 		Restaurant restaurant = restaurantRepository.findById(adReqDto.getRestaurantId())
 			.orElseThrow(()-> new EntityNotFoundException("가게를 찾을 수 없습니다."));
+
+		Manager manager = managerRepository.findById(verifiedMember.id())
+			.orElseThrow(() -> new IllegalArgumentException("광고 생성 권한이 없습니다."));
+
+		if(!restaurant.getManager().getId().equals(manager.getId())){
+			throw new IllegalArgumentException("사장님 가게가 아닙니다. 광고를 생성할 권한이 없습니다.");
+		}
 
 		Ad ad = Ad.builder()
 			.restaurant(restaurant)
@@ -41,8 +57,7 @@ public class AdService {
 
 	@Transactional
 	public void activeAds(Long adId) {
-		Ad ad = adRepository.findById(adId)
-			.orElseThrow(() -> new EntityNotFoundException("광고를 찾을 수 없습니다."));
+		Ad ad = findAdOrThrow(adId);
 
 		ad.activeAds();
 		adRepository.save(ad);
@@ -50,8 +65,7 @@ public class AdService {
 
 	@Transactional
 	public void inActiveAds(Long adId) {
-		Ad ad = adRepository.findById(adId)
-			.orElseThrow(() -> new EntityNotFoundException("광고를 찾을 수 없습니다."));
+		Ad ad = findAdOrThrow(adId);
 
 		ad.inActiveAds();
 		adRepository.save(ad);
