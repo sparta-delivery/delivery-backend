18ee496281aaf1c4a8e436aa91da4ec6a47b031f
kimwanyoung <dhks2869@gmail.com>
2024-11-06 11:29:23 +0900

    test : 사장 답글 생성 테스트

--- src/test/java/com/sparta/deliverybackend/domain/restaurant/service/CommentServiceTest.java
+++ src/test/java/com/sparta/deliverybackend/domain/restaurant/service/CommentServiceTest.java
@@ -3,6 +3,8 @@ package com.sparta.deliverybackend.domain.restaurant.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,11 +14,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.sparta.deliverybackend.api.auth.controller.dto.VerifiedMember;
+import com.sparta.deliverybackend.domain.member.entity.Manager;
 import com.sparta.deliverybackend.domain.member.entity.Member;
+import com.sparta.deliverybackend.domain.member.repository.ManagerRepository;
 import com.sparta.deliverybackend.domain.member.service.MemberService;
 import com.sparta.deliverybackend.domain.order.entity.Order;
 import com.sparta.deliverybackend.domain.order.entity.OrderStatus;
-import com.sparta.deliverybackend.domain.order.repository.OrderMenuRepository;
 import com.sparta.deliverybackend.domain.order.service.OrderService;
 import com.sparta.deliverybackend.domain.restaurant.controller.dto.CommentCreateReqDto;
 import com.sparta.deliverybackend.domain.restaurant.controller.dto.CommentRespDto;
@@ -37,7 +40,7 @@ class CommentServiceTest {
 	private OrderService orderService;
 
 	@Mock
-	private OrderMenuRepository orderMenuRepository;
+	private ManagerRepository managerRepository;
 
 	@InjectMocks
 	private CommentService commentService;
@@ -131,5 +134,75 @@ class CommentServiceTest {
 		assertThatThrownBy(() -> commentService.createComment(req, verifiedMember, orderId))
 			.isInstanceOf(ResponseStatusException.class)
 			.hasMessageContaining("권한이 없습니다.");
+	}
+
+	@Test
+	@DisplayName("사장 답글 정상 생성 테스트")
+	public void create_manager_comment_success_test() {
+		// given
+		Long orderId = 1L;
+		CommentCreateReqDto req = new CommentCreateReqDto("comments");
+		VerifiedMember verifiedMember = new VerifiedMember(1L);
+		Manager manager = Manager.builder()
+			.id(verifiedMember.id())
+			.build();
+		Restaurant restaurant = Restaurant.builder()
+			.id(1L)
+			.manager(manager)
+			.build();
+		Member member = Member.builder()
+			.id(2L)
+			.build();
+		Order order = Order.builder()
+			.id(orderId)
+			.member(member)
+			.orderStatus(OrderStatus.COMPLETE)
+			.build();
+		Comment comment = Comment.builder()
+			.id(1L)
+			.member(member)
+			.restaurant(restaurant)
+			.order(order)
+			.contents(req.content())
+			.build();
+
+		when(managerRepository.findById(any())).thenReturn(Optional.of(manager));
+		when(commentRepository.findById(any())).thenReturn(Optional.of(comment));
+
+		//when
+		CommentRespDto managerComment = commentService.createManagerComment(req, verifiedMember, comment.getId());
+
+		//then
+		assertThat(managerComment.getReply()).isEqualTo("comments");
+	}
+
+	@Test
+	@DisplayName("사장 답글 작성 시 리뷰가 없으면 예외가 발생한다")
+	public void create_manager_comment_exception_test() {
+		// given
+		CommentCreateReqDto req = new CommentCreateReqDto("comments");
+		VerifiedMember verifiedMember = new VerifiedMember(1L);
+		when(commentRepository.findById(any())).thenReturn(Optional.empty());
+
+		//when
+		assertThatThrownBy(() -> commentService.createManagerComment(req, verifiedMember, 1L))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("존재하지 않는 리뷰입니다.");
+	}
+
+	@Test
+	@DisplayName("존재하지 않는 사장이면 예외발생")
+	public void not_exist_manager_create_comment_exception_test() {
+		// given
+		CommentCreateReqDto req = new CommentCreateReqDto("comments");
+		VerifiedMember verifiedMember = new VerifiedMember(1L);
+
+		when(commentRepository.findById(any())).thenReturn(Optional.of(new Comment()));
+		when(managerRepository.findById(any())).thenReturn(Optional.empty());
+
+		//when
+		assertThatThrownBy(() -> commentService.createManagerComment(req, verifiedMember, 1L))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("존재하지 않는 사장입니다.");
 	}
 }
