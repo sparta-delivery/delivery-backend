df1d84c218db9ac944b596c41261b679a1137ab4
kimwanyoung <dhks2869@gmail.com>
2024-11-06 09:36:19 +0900

    feat : 댓글 컨트롤러 작성

---
+++ src/main/java/com/sparta/deliverybackend/domain/restaurant/controller/CommentController.java
@@ -0,0 +1,73 @@
+package com.sparta.deliverybackend.domain.restaurant.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sparta.deliverybackend.api.auth.controller.dto.VerifiedMember;
+import com.sparta.deliverybackend.domain.restaurant.controller.dto.CommentCreateReqDto;
+import com.sparta.deliverybackend.domain.restaurant.controller.dto.CommentRespDto;
+import com.sparta.deliverybackend.domain.restaurant.controller.dto.CommentUpdateReqDto;
+import com.sparta.deliverybackend.domain.restaurant.service.CommentService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/orders/{orderId}/comments")
+public class CommentController {
+
+	private final CommentService commentService;
+
+	@PostMapping
+	public ResponseEntity<CommentRespDto> createComment(
+		@PathVariable Long orderId,
+		@RequestBody CommentCreateReqDto req,
+		VerifiedMember verifiedMember
+	) {
+		return ResponseEntity
+			.status(HttpStatus.CREATED)
+			.body(new CommentRespDto());
+	}
+
+	@PostMapping("/{commentId}/manager-comment")
+	public ResponseEntity<CommentRespDto> createManagerComment(
+		@PathVariable Long orderId,
+		@PathVariable Long commentId,
+		@RequestBody CommentCreateReqDto req,
+		VerifiedMember verifiedMember
+	) {
+		return ResponseEntity
+			.status(HttpStatus.CREATED)
+			.body(new CommentRespDto());
+	}
+
+	@PatchMapping("/{commentId}")
+	public ResponseEntity<Void> updateComment(
+		@PathVariable Long orderId,
+		@PathVariable Long commentId,
+		@RequestBody CommentUpdateReqDto req,
+		VerifiedMember verifiedMember
+	) {
+		return ResponseEntity
+			.status(HttpStatus.NO_CONTENT)
+			.build();
+	}
+
+	@DeleteMapping("/{commentId}")
+	public ResponseEntity<Void> deleteComment(
+		@PathVariable Long orderId,
+		@PathVariable Long commentId,
+		VerifiedMember verifiedMember
+	) {
+		return ResponseEntity
+			.status(HttpStatus.NO_CONTENT)
+			.build();
+	}
+}
---
+++ src/main/java/com/sparta/deliverybackend/domain/restaurant/controller/dto/CommentCreateReqDto.java
@@ -0,0 +1,5 @@
+package com.sparta.deliverybackend.domain.restaurant.controller.dto;
+
+public class CommentCreateReqDto {
+	private String content;
+}
