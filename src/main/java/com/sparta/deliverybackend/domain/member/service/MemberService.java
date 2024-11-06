3cdc77ef5a26e57bafbfcd49577fc8947a6c5d3a
wootaepark <pakdal99@gmail.com>
2024-11-06 15:43:48 +0900

    feat : Member 예외 처리 구현

--- src/main/java/com/sparta/deliverybackend/domain/member/service/MemberService.java
+++ src/main/java/com/sparta/deliverybackend/domain/member/service/MemberService.java
@@ -6,6 +6,8 @@ import com.sparta.deliverybackend.api.auth.controller.dto.VerifiedMember;
 import com.sparta.deliverybackend.domain.member.controller.dto.MemberUpdateReqDto;
 import com.sparta.deliverybackend.domain.member.entity.Member;
 import com.sparta.deliverybackend.domain.member.repository.MemberRepository;
+import com.sparta.deliverybackend.exception.customException.NotFoundEntityException;
+import com.sparta.deliverybackend.exception.enums.ExceptionCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,12 +19,12 @@ public class MemberService {
 
 	public Member findMember(Long id) {
 		return memberRepository.findById(id)
-			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+			.orElseThrow(() -> new NotFoundEntityException(ExceptionCode.NOT_FOUND_MEMBER));
 	}
 
 	public void update(Long id, MemberUpdateReqDto req, VerifiedMember verifiedMember) {
 		Member member = memberRepository.findById(id)
-			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+			.orElseThrow(() -> new NotFoundEntityException(ExceptionCode.NOT_FOUND_MEMBER));
 		member.validateAuthority(verifiedMember.id());
 		member.update(req);
 		memberRepository.save(member);
@@ -30,7 +32,7 @@ public class MemberService {
 
 	public void delete(Long id, VerifiedMember verifiedMember) {
 		Member member = memberRepository.findById(id)
-			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+			.orElseThrow(() -> new NotFoundEntityException(ExceptionCode.NOT_FOUND_MEMBER));
 		member.validateAuthority(verifiedMember.id());
 		member.delete();
 		memberRepository.save(member);
